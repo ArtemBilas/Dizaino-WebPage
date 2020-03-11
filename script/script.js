ca4dd1c994ce96125e444fabee58afea69c2ca5c
ArtemBilas <artembilas@gmail.com>
2020-03-11 14:48:01 +0200

    dizaino-webpage

---
+++ script/script.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+
+    let btnMenu = document.querySelector("#btnMenu").onclick = function(){
+        let x = document.querySelector("#myTopnav");
+        x.classList.toggle("responsive");
+    }
+}
